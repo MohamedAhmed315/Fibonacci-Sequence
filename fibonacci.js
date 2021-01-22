6e7ac88016a69b97ba7aedae7203c1602236e463
Mohamed Ahmed <mohamedahmed315@hotmail.com>
2021-01-22 18:39:40 +0200

    Add files via upload

---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+
+let fibonacci = function(end){
+
+    let array1 = [];
+
+    array1[0] = 1;
+    array1[1] = 1;
+
+    for (let i = 2; i < end; i++){
+        array1[i] = array1[i-1] + array1[i-2];
+    }
+    return array1;
+}
+
+console.log("Fibonacci Series = " + fibonacci(12) + ",...");
